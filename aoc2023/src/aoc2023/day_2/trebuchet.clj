6f3f6f44241dc1aa4680de31cb16ef5a2fa2406f
nlhnt <grimonce@gmail.com>
2023-12-31 17:32:03 +0100

    complete reverse digits map using reduce

--- aoc2023/src/aoc2023/day_2/trebuchet.clj
+++ aoc2023/src/aoc2023/day_2/trebuchet.clj
@@ -6,7 +6,7 @@
   (->> (io/reader file-path) ; Open a reader for the file
        (line-seq) ; Read lines as a lazy sequence
        (vec) ; Convert the lazy sequence to a vector
-  ))
+       ))
 
 (def file-path "src/aoc2023/day_1/input") ; Define filepath
 
@@ -14,20 +14,66 @@
 
 (defn find-first-two-numbers [input-str]
   (let [numbers (re-seq #"\d+" input-str)]
-    (take 2 (map #(Integer. %) numbers)))
-  )
+    (take 2 (map #(Integer. %) numbers))))
 
 (defn find-first-two-digits [input-str]
   (let [numbers (re-seq #"\d" input-str)]
     (take 2 (map #(Integer. %) numbers))))
 
+(def digits-map {"zero" "0"
+                 "one" "1"
+                 "two" "2"
+                 "three" "3"
+                 "four" "4"
+                 "five" "5"
+                 "six" "6"
+                 "seven" "7"
+                 "eight" "8"
+                 "nine" "9"})
+
+(doseq [[k v] digits-map] (
+                           (println {(apply str (reverse k)) v}) 
+                          ;;  (println k)
+                           ))
+
+(doseq [[k v] {:one "two" :three "four"}] (println "Found key" k "with value" v))
+
+(doseq [[k v] {:one "two" :three "four"}] (println "Found key" (apply str (reverse (str k))) "with value " v) )
+
+
+(doseq [[k v] digits-map] (println "Found key" (apply str (reverse (str k))) "with value " v))
+
+(def reverse-digits-map
+  (reduce (fn [extended-digits-map [k v]]
+            (assoc extended-digits-map (apply str (reverse k)) v))
+          {}
+          digits-map)
+)
+reverse-digits-map
+
+(def full-digits-map (
+                      conj digits-map reverse-digits-map
+))
+full-digits-map
+
+(doseq [[k v] digits-map] (
+                           let [new-map digits-map] (
+                                                     (conj new-map {(apply str (reverse k)) v})
+                                                     (println new-map))))
+
+(doseq [el digits-map] (println el))
+
+;; (doseq [el [1 2 3]] (println el))
+
+(get digits-map "zero")
+
+(def digits-regex #"\d|zero|one|two|three|four|five|six|seven|eight|nine")
+
+(println digits-regex)
+
 (defn get-first-and-last-digits-spelled-too [input-str]
   (let [numbers (re-seq #"\d|zero|one|two|three|four|five|six|seven|eight|nine" input-str)]
-     [
-      (first numbers) (last numbers)
-      ]
-    )
-  )
+    [(first numbers) (last numbers)]))
 
 (defn spelled-digit-to-number [spelled-digit]
   (case (s/lower-case spelled-digit)
@@ -52,12 +98,10 @@
   (apply str numbers))
 
 (defn process-line [input-str]
-  ((comp concatenate-numbers get-first-and-last-digits) input-str)
-  )
+  ((comp concatenate-numbers get-first-and-last-digits) input-str))
 
 (defn sum-calibration-values [dirty-calibration-sequence]
-  ((comp + (map process-line)) dirty-calibration-sequence)
-  )
+  ((comp + (map process-line)) dirty-calibration-sequence))
 
 (def all-two-digit-numbers (map #(Integer. %) (map concatenate-numbers (map get-first-and-last-digits lines-vector))))
 
